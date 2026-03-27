f82735bd25dcaf15cfb8e2d33d92060bd676ffe7
barangandevon <barangandevon@gmail.com>
2026-03-27 11:11:11 +0800

    chore: layout modify

--- components/AdminNavbar.tsx
+++ components/AdminNavbar.tsx
@@ -1,12 +1,30 @@
+import { Bell, Calendar } from "lucide-react";
+
 const AdminNavbar = () => {
+  const now = new Date();
+  const formattedDate = now.toLocaleDateString("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
+
   return (
-    <header className="h-16 flex items-center justify-between px-8 border-b border-base-300 bg-base-100">
-      <h2 className="text-xl font-bold">Admin Dashboard</h2>
-
+    <header className="h-16 flex items-center justify-between  bg-base-100">
+      <h2 className="text-xl font-bold">Official Admin Dashboard</h2>
+      <input
+        type="text"
+        placeholder="Search"
+        className="input flex-1 mx-6 h-8"
+      />
       <div className="flex items-center gap-3">
-        <button className="btn btn-ghost btn-circle">🔔</button>
+        <span className="btn btn-ghost btn-circle">
+          <Bell />
+        </span>
 
-        <button className="btn btn-ghost">📅 Oct 27, 2023</button>
+        <span className="inline-flex gap-2">
+          <Calendar /> {formattedDate}
+        </span>
       </div>
     </header>
   );
